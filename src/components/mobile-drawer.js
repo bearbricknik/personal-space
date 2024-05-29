4349138da77bb19bfd636f46d0b5cb4608793da9
bearbricknik <dominik.huber97@googlemail.com>
2024-05-29 22:16:41 +0200

    Update mobile-drawer.js

--- src/components/mobile-drawer.js
+++ src/components/mobile-drawer.js
@@ -1,7 +1,7 @@
 import { MenuContent } from '@/components/menu-content'
 import { Button } from '@/components/ui/button'
 import { Drawer, DrawerContent, DrawerTrigger } from '@/components/ui/drawer'
-import { PanelRightClose } from 'lucide-react'
+import { PanelTopClose } from 'lucide-react'
 
 export function MobileDrawer() {
   return (
@@ -9,7 +9,7 @@ export function MobileDrawer() {
       <DrawerTrigger asChild>
         <div className="flex rounded-lg bg-gray-100">
           <Button variant="ghost" size={"icon"} title="Toggle menu">
-            <PanelRightClose size={18} />
+            <PanelTopClose size={18} />
           </Button>
           <Button className="gap-1" variant="ghost">
             <span>Dominik Huber</span>
