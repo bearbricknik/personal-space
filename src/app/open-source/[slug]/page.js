f9643a9f11102d8449ec23725b8c29d803050a71
bearbricknik <dominik.huber97@googlemail.com>
2024-12-19 08:11:21 +0100

    params

--- src/app/open-source/[slug]/page.js
+++ src/app/open-source/[slug]/page.js
@@ -1,14 +1,24 @@
-"use client"
 
-function page(params) {
 
-    console.log(params.slug)
+import { ScrollArea } from '@/components/scroll-area'
+import { FloatingHeader } from '@/components/floating-header'
+import { PageTitle } from '@/components/page-title'
+import { GradientBg3 } from '@/components/gradient-bg'
+
+export default async function OS(params, searchParams) {
+    console.log(params)
+    console.log(searchParams)
 
     return (
-        <div>
-            {params.slug}
-        </div>
+        <ScrollArea useScrollAreaId>
+            <GradientBg3 />
+            <FloatingHeader scrollTitle="Open Source" />
+            <div className="content-wrapper">
+                <div className="content">
+                    <PageTitle title={1} />
+                    <p>Blog content {JSON.stringify(params)}</p>
+                </div>
+            </div>
+        </ScrollArea>
     )
 }
-
-export default page
