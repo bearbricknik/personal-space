a1310ec51e2dc332fde9dff888f3f3cecd81eb4f
bearbricknik <dominik.huber97@googlemail.com>
2025-01-09 22:11:26 +0100

    test

---
+++ src/content/open-source/pressable-button-preview.js
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+function PressableButtonPreview() {
+  return (
+    <div className="flex min-h-[200px] w-full items-center justify-center">
+      <Button>
+        ProfitPath
+      </Button>
+    </div>
+  )
+}
+
+export default PressableButtonPreview
